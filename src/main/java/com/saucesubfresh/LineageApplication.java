bedec51f1f94761105579f973667845077550071
lijunping <2544054976@qq.com>
2023-04-29 19:48:00 +0800

    :tada: 项目初始化

---
+++ src/main/java/com/saucesubfresh/LineageApplication.java
@@ -0,0 +1,15 @@
+package com.saucesubfresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: 李俊平
+ * @Date: 2023-04-29 19:42
+ */
+@SpringBootApplication
+public class LineageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LineageApplication.class, args);
+    }
+}
---
+++ src/main/java/com/saucesubfresh/lineage/controller/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author: 李俊平
+ * @Date: 2023-04-29 19:44
+ */
+package com.saucesubfresh.lineage.controller;
